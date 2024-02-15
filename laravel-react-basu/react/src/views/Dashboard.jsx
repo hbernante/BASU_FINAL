23bad1d4466c0b33840090e4fb9f4e0738bdf4cf
Zel <hbernante@student.apc.edu.ph>
2024-02-15 21:06:35 +0800

    Initial Commit

---
+++ laravel-react-basu/react/src/views/Dashboard.jsx
@@ -0,0 +1,13 @@
+import PageComponent from "../components/PageComponent";
+
+import React from 'react';
+
+export default function Dashboard() {
+  return (
+    <PageComponent title="Dashboard">
+       <div className="App">
+      Work In Progress
+    </div>
+    </PageComponent>
+  )
+}
---
+++ laravel-react-basu/routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ReservationController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::post('/reservations', [ReservationController::class, 'store']);
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::apiResource('reservation',ReservationController::class);
+});
+
+Route::post('/signup', [AuthController::class,'signup']);
+Route::post('/login', [AuthController::class,'login']);
+
+
